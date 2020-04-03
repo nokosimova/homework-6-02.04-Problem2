98f039354130c24e034010ad0b6562e4c6fa1893
unknown <nkosimova06@gmail.com>
2020-04-03 20:57:32 +0500

    added Show methods

--- Program.cs
+++ Program.cs
@@ -15,17 +15,32 @@ namespace Proj2
         class Title
         {
             public string TitleName{get; set;}
+            public void Show() 
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Title: {this.TitleName}");
+            }
+
 
         }
         class Content
         {
             public string ContentName{get; set;}
+            public void Show() 
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"Title: {this.ContentName}");
+            }
 
         }
         class Author
         {
             public string AuthorName{get; set;}
-
+            public void Show() 
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine($"Title: {this.AuthorName}");
+            }
         }
     }
 }

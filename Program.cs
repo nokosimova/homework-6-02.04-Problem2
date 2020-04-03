fbafc61ccadab8f03638e91e72cf9ce7ffe5d6f2
unknown <nkosimova06@gmail.com>
2020-04-03 20:53:56 +0500

    creating all classes with one field

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Proj2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        class Book
+        {
+
+        }
+        class Title
+        {
+            public string TitleName{get; set;}
+
+        }
+        class Content
+        {
+            public string ContentName{get; set;}
+
+        }
+        class Author
+        {
+            public string AuthorName{get; set;}
+
+        }
+    }
+}
